90e552be9a70938a05db3252a4f90ab1bd01be76
Neeraj Singh <869neerajsingh@gmail.com>
2025-01-20 20:43:07 +0530

    refactor: Seperate method to find winner in utils folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,7 @@
 import { useState } from "react";
 
-import { BoardProps, SquareProps, Squares, SquareValue } from "./types";
-
-function findWinner(squares: Squares): SquareValue {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (const line of lines) {
-    const [a, b, c] = line;
-
-    if (squares[a] === squares[b] && squares[b] === squares[c]) {
-      return squares[a];
-    }
-  }
-
-  return null;
-}
+import { BoardProps, SquareProps, Squares } from "./types";
+import { findWinner } from "./utils/findWinner";
 
 function Square({ value, onSquareClick }: SquareProps) {
   console.log(value);
---
+++ src/utils/findWinner.ts
@@ -0,0 +1,24 @@
+import { Squares, SquareValue } from "../types";
+
+export function findWinner(squares: Squares): SquareValue {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (const line of lines) {
+    const [a, b, c] = line;
+
+    if (squares[a] === squares[b] && squares[b] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
